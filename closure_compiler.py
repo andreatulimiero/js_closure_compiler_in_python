8459050814bc9e20c8c6f427e1440c46dc504c85
Tuly- <tulimiero.andrea@gmail.com>
2016-10-16 13:30:31 +0200

    Added colors to output. Requirements have been updated

--- closure_compiler.py
+++ closure_compiler.py
@@ -1,5 +1,7 @@
 import os, requests, json
 from functools import reduce
+from termcolor import colored
+import colorama
 
 # Default settings
 config = {
@@ -9,7 +11,26 @@ config = {
   'output_info' : 'compiled_code',
   'output_file' : 'app.min.js'
 }
+
 files_names = []
+
+def print_info(info):
+  print( colored(info, 'white') )
+
+def print_success(success):
+  print( colored(success, 'green'))
+
+def print_warning(warning):
+  print( colored(warning, 'yellow') )
+
+def print_error(error):
+  print( colored(error, 'red') )
+  
+def print_filename(filename):
+  print( colored(filename, 'grey') )
+
+def print_stat(stat):
+  print( colored(stat, 'cyan') )
 
 def switch_to_current_dir():
   abspath = os.path.abspath(__file__)
@@ -17,21 +38,21 @@ def switch_to_current_dir():
   os.chdir(dname)
 
 def load_configs():
-  print('Loading configurations ...')
+  print_info('Loading configurations ...')
   try:
     f = open('.closure_compiler_config.json')
     try:
       loaded_config = json.loads(f.read().strip())
     except json.JSONDecodeError:
-      print('#Malformed config file, using defaults ...')
+      print_warning('#Malformed config file, using defaults ...')
       return
     for key in loaded_config:
       if key not in config:
-        print('#' + key + ' is not a valid configuration')
+        print_warning('#' + key + ' is not a valid configuration')
       else:
         config[key] = loaded_config[key]
   except FileNotFoundError:
-    print('#No .closure_compile_config.json file found. Using default values')
+    print_warning('#No .closure_compile_config.json file found. Using default values')
 
 def get_js_code():
   print('\nReading files specified in .to_closure_compile ...')
@@ -43,14 +64,15 @@ def get_js_code():
         f = open(filename.strip())
         joined_js += f.read()
         files_names.append(filename.strip())
-        print('|-' + filename)
+        print_filename('|-' + filename)
       except FileNotFoundError:
-        print('|-No ' + filename + ' file found')
+        print_error('|-No ' + filename + ' file found')
   except FileNotFoundError:
-    print('#No .to_closure_compile file found')
+    print_warning('#No .to_closure_compile file found')
   return joined_js
 
 if __name__ == '__main__':
+  colorama.init()
   switch_to_current_dir()
   load_configs()
   data = {
@@ -60,22 +82,22 @@ if __name__ == '__main__':
     'output_info' : config['output_info']
   }
 
-  print('\nRequesting compilation ...')
+  print_info('\nRequesting compilation ...')
   if not len(data['js_code']):
-    print('#No file specified, aborting compilation\n')
+    print_error('#No file specified, aborting compilation\n')
     exit(2)
     
   try:
     r = requests.post(config['url'], data=data)    
   except:
-    print('Impossible to communicate with the server, check your internet connection')
+    print_error('Impossible to communicate with the server, check your internet connection')
     exit(1)
   with open(config['output_file'], mode='w+') as output_file:
     print(r.text, file=output_file)
-    print('Compilation saved in ' + config['output_file'] + '\n')
+    print_success('Compilation saved in ' + config['output_file'] + '\n')
     current_size = os.path.getsize(config['output_file'])//1024
     previous_size = reduce(lambda x,y: x + y, [os.path.getsize(x) for x in files_names])//1024
-    print('Previous size: ' + str(previous_size) + ' Kb')
-    print('Current size: ' + str(current_size) + ' Kb')
-    print('Saved: ' + str(previous_size - current_size) + 'Kb')
+    print_info('Previous size: ' + str(previous_size) + ' Kb')
+    print_info('Current size: ' + str(current_size) + ' Kb')
+    print_stat('Saved: ' + str(int(current_size/previous_size*100)) + '% (' + str(previous_size - current_size) + ' Kb) of room')
     exit(0)
